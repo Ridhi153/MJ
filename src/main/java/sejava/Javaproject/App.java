32856e487e5df29b192fc7e34004fed548d5c043
ridhi <ridhisriramula@gmail.com>
2024-12-29 18:04:16 +0530

    First commit

---
+++ src/main/java/sejava/Javaproject/App.java
@@ -0,0 +1,11 @@
+package sejava.Javaproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java Maven Project");
+    }
+}
